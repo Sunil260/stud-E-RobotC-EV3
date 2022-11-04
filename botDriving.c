64f0aea07cc1166e8887bee2c15e9ba4ab0f4203
rita-sun <ritanuo@gmail.com>
2022-11-04 14:27:09 -0400

    Update botDriving.c
    i changed stuff

--- botDriving.c
+++ botDriving.c
@@ -1,12 +1,13 @@
 /*
-Group 28 
-Driving code 
+FUNCTIONS STILL NEEDED:
+Dispenser stuff
+Test mode stuff
+
+ALSO MAKE SURE TO SWITCH ALL SENSOR PORTS TO MATCH OUR ROBOT
 */
 
-void turnRobot()
-{
 
-}
+
 // function to configure all sensors
 void configureAllSensors () 
 {
@@ -33,7 +34,7 @@ void drivePower(int motorPower) {
 	return;
 }
 
-// driveDist cm function
+// drive a certain distance in cm function
 void driveDist(float distCM, int motorPow) {
 	const int encoderLimit=distCM*180/(PI*2.75);
 	nMotorEncoder[motorB]=0;
@@ -43,7 +44,7 @@ void driveDist(float distCM, int motorPow) {
 	motor[motorB]=motor[motorC]=0;
 }
 
-// stop drive function
+// stop driving function
 void driveStop() {
 	motor[motorA]=motor[motorD]=0;
 	return;
@@ -61,76 +62,190 @@ wait1Msec(2000);
 return;	
 }
 
-
 // function to choose the mode
-int modeChoose () {
+int modeChoose() {
 // input verification
- while(!(mode==1||mode==2)) {
-int mode=0;
-	
-displayString (5, “Choose your mode:”);
-displayString (7, “Press left button for Study Mode”);
-displayString (8, “Press right button for Test Mode”);
+	int mode=0;
+	while(!(mode==1||mode==2)) 
+	{
+		displayString (5, "Choose your mode:");
+		displayString (7, "Press LEFT button for:");
+		displayString (8, "Study Mode");
+		displayString (10, "Press RIGHT button for:");
+		displayString (11, "Test Mode");
 // study mode=1, test mode=2
-if (getButtonPress==LEFT_BUTTON) {
-mode=1;
-}
+		if (getButtonPress(LEFT_BUTTON)) 
+			{
+			mode=1;
+			}
 
-if (getButtonPress==RIGHT_BUTTON) {
-mode=2;
-}
-}
-return mode;
-}
-// follow a line function
-void followLine (int tapeColour) {
-	while (SensorValue[]==tapeColour) {
-		drivePower(20);
+		if (getButtonPress(RIGHT_BUTTON)) 
+			{
+			mode=2;
+			}
 	}
-	driveDist(howeverlong, 20);
-	turnCW(90,20);
-	return;
-} 
-
-// follow line in a square function
-void lineLoop() {
-for (int count=1; count<=4; count++) {
-	followLine(int(colorRed));
-}
-	return;
+	eraseDisplay();
+	return mode;
 }
 
-
-
-
-// main function
-task main () {
-configureAllSensors();
-
-while (!done) {
-bool modeInterface=true;
-while (modeInterface==true) {
-	modeChoose();
-	modeInterface=false;
-}
-
-while (mode==1) {
-	//pnrng
-}
-
-	while (mode==2) {
-	//pnrng
-}
-
-	// press enter button to go back to the menu
-	if (getButtonPress==ENTER_BUTTON) {
-		modeInterface=true;
+// dispense function someone make the menu
+void dispense () {
+	displayString(5, “SKITTLE MENU”);
+	displayString(8, “Press down to scroll.”);
+	displayString(9, “Press enter to scroll.”);
+	int selection=0;
+	while (!(selection==1||selection==2||selection==3||selection==4)) 
+	{
 	}
-}
+	eraseDisplay();
 return;
 }
 
-task main()
+// follow a line function
+void followLine (int tapeColour) {
+	} 
+
+ 
+void lineLoop() {
+	for (int count=1; count<=4; count++) 
+	{
+		followLine(int(colorRed));
+	}
+	return;
+}
+
+// study mode
+void studyMode (int tapeColour, int &lapCount) 
+{
+	lapCount++;
+	for (int count=0; count<4; count++) 
+	{
+		drivePower(20);
+		while (SensorValue[]==tapeColour) 
+		{
+			if (SensorValue[touchPort]==0) 
+			{
+				driveStop();
+				dispense();
+				lapCount=0;
+			}
+			drivePower(20);
+		}
+		driveStop();
+		driveDist(howeverlong, 20);
+		turnCW(90,20);
+	}
+	return;
+}
+
+void fireBullet() 
 {
 
+}
+
+// test mode function
+void testMode() 
+{ 
+// timer
+	displayString (5,"Press Up to Begin the Timer");
+	displayString (7,"Press Up Again to Stop the Timer");
+	while(!getButtonPress(UP_BUTTON)) 
+	{
+	}
+    while(getButtonPress(UP_BUTTON)) 
+	{
+	}
+	eraseDisplay();
+	time1[T1]=0;
+	while(!getButtonPress(UP_BUTTON)) 
+	{
+	}
+    while(getButtonPress(UP_BUTTON)) 
+	{
+	}
+	displayString (5, "Your time is %f", time1[T1]/1000.0);
+
+// file for answers
+	TFileHandle fileIn;
+	bool fileOkay = openReadPC(fileIn, “answers.txt”);
+	if (!fileOkay) 
+	{
+		displayString(5,”Error!”);
+		wait1Msec(5000);
+	}
+	else 
+	{
+		int questionNum=0;
+		int score = 0;
+		readCharPC (fileIn,questionNum);
+		for (int line=0; line<questionNum; line++) 
+		{
+			char answer = ’ ’;
+			readCharPC (fileIn,answer);
+			// insert how to DRIVE and SCAN
+			// need to declare variable input somewhere
+			if (input==answer) 
+			{
+				score++;
+			}
+		}
+	driveStop();
+	float grade=float(score)/questionNum;
+	displayString (5, “You scored %d/%d, score, questionNum);
+		displayString( (6, “%f”, grade)
+		if (grade<80) 
+		{
+			fireBullet(); // need to create this function
+		}
+	}
+return;
+}
+
+
+// main function
+#include “PC_FileIO.c”
+
+task main ()
+{
+	configureAllSensors();
+
+	int done=1;
+
+	while (done!=0) 
+	{
+		bool modeInterface=true;
+		while (modeInterface==true) 
+		{
+			int mode = modeChoose();
+			modeInterface=false;
+		}
+
+		while (mode==1) {
+			// something is defo wrong its not making sense in my head
+			int lapCount=0;
+			studyMode(int(colorRed), lapCount);
+			if (lapCount>2) {
+				done=0;
+			}
+		}
+
+		while (mode==2) 
+		{
+			testMode();
+			time1[T2]=0;
+			if ((time1[T2]/1000.)>120) 
+			{
+				done=0;
+			}
+			
+		}
+
+		// press enter button to go back to the menu this defo doesnt work like i want it so that at any point if u ress it it goes back to the 
+		// main menu
+		if (getButtonPress==ENTER_BUTTON) 
+		{
+			modeInterface=true;
+		}
+	}
+	return;
 }
