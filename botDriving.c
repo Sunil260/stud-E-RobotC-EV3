fb7de6bf45ce294d20869158858cda41213a6018
JordanT-MWang <116920132+JordanT-MWang@users.noreply.github.com>
2022-11-17 19:35:07 -0500

    lapcount

--- botDriving.c
+++ botDriving.c
@@ -8,29 +8,35 @@ ALSO MAKE SURE TO SWITCH ALL SENSOR PORTS TO MATCH OUR ROBOT
 
 
 
+const string GYRO_PORT = "S1";
+const string COLOR_PORT = "S2";
+const string ULTRASONIC_PORT = "S3";
+const string TOUCH_PORT = "S4";
+
 // function to configure all sensors
-void configureAllSensors () 
+void configureAllSensors ()
 {
-	SensorType[S1] = sensorEV3_Touch; 
-	wait1MSec(50); 
-	SensorType[S2] = sensorEV3_Ultrasonic; 
-	wait1MSec(50);
-	SensorType[S3] = sensorEV3_Color; 
-	wait1MSec(50);
-	SensorType[S4] = sensorEV3_Gyro; 
-	wait1MSec(50);
-	SensorMode[S3] = modeEV3Color_Color; 
-	wait1MSec(50);
-	SensorMode[S4] = modeEV3Gyro_Calibration; 
-	wait1MSec(50); 
-	SensorMode[S4] = modeEV3Gyro_RateAndAngle; 
-	wait1MSec(50);
+	SensorType[S1] = sensorEV3_Touch;
+	wait1Msec(50);
+	SensorType[S2] = sensorEV3_Ultrasonic;
+	wait1Msec(50);
+	SensorType[S3] = sensorEV3_Color;
+	wait1Msec(50);
+	SensorType[S4] = sensorEV3_Gyro;
+	wait1Msec(50);
+	SensorMode[S3] = modeEV3Color_Color;
+	wait1Msec(50);
+	SensorMode[S4] = modeEV3Gyro_Calibration;
+	wait1Msec(50);
+	SensorMode[S4] = modeEV3Gyro_RateAndAngle;
+	wait1Msec(50);
 	return;
 }
 
 // drive function
 void drivePower(int motorPower) {
-	motor[motorA]=motor[motorD]=motorPower;
+	motor[motorA] = motorPower;
+	motor[motorD] = -motorPower;
 	return;
 }
 
@@ -52,21 +58,21 @@ void driveStop() {
 
 // turn clockwise function
 void turnCW(int angle, int motorPower) {
-	resetGyro(S1);
-motor[motorA] = -motorPower;
-motor[motorD] = motorPower;
-while(SensorValue[S1] < angle)
-{ }
-driveStop();
-wait1Msec(2000);
-return;	
+	resetGyro(0);
+	motor[motorA] = motorPower;
+	motor[motorD] = motorPower;
+	while(SensorValue[GYRO_PORT] < angle)
+	{ }
+	driveStop();
+	wait1Msec(2000);
+	return;
 }
 
 // function to choose the mode
 int modeChoose() {
 // input verification
-	int mode=0;
-	while(!(mode==1||mode==2)) 
+	int mode = 0;
+	while(!(mode==1||mode==2))
 	{
 		displayString (5, "Choose your mode:");
 		displayString (7, "Press LEFT button for:");
@@ -74,12 +80,12 @@ int modeChoose() {
 		displayString (10, "Press RIGHT button for:");
 		displayString (11, "Test Mode");
 // study mode=1, test mode=2
-		if (getButtonPress(LEFT_BUTTON)) 
+		if (getButtonPress(LEFT_BUTTON))
 			{
 			mode=1;
 			}
 
-		if (getButtonPress(RIGHT_BUTTON)) 
+		if (getButtonPress(RIGHT_BUTTON))
 			{
 			mode=2;
 			}
@@ -90,78 +96,77 @@ int modeChoose() {
 
 // dispense function someone make the menu
 void dispense () {
-	
+
 }
 
 // study mode
-void studyMode (int tapeColour, int turnColour, int &lapCount) 
+void studyMode (int tapeColour, int turnColour, int &lapCount)
 {
-	lapCount++;
-	for (int count=0; count<4; count++) 
+	for (int count=0; count<4; count++)
 	{
 		drivePower(10);
-		while (SensorValue[]==tapeColour) 
+		while (SensorValue[COLOR_PORT] == tapeColour)
 		{
-			if (SensorValue[touchPort]==0) 
+			if (SensorValue[TOUCH_PORT] == 0)
 			{
 				driveStop();
 				dispense();
-				lapCount=0;
 			}
-			drivePower(20);
 		}
-		while (!(sensorValue[]==tapeColour||sensorValue[]==turnColour)) {
+		while (!(SensorValue[COLOR_PORT]==tapeColour||SensorValue[COLOR_PORT]==turnColour)) {
 			motor[motorA]=motor[motorD]=0;
-			playSoundFile("fixSound.rsf");
+			playSoundFile("Uh-oh.rsf");
 		}
 		driveStop();
-		driveDist(howeverlong, 20);
 		turnCW(90,20);
+		wait1Msec(2000);
 	}
+	lapCount++;
 	return;
 }
 
-void fireBullet() 
+
+void fireBullet()
 {
 
 }
 
-float timer() 
+float timer()
 {
 	displayString (5,"Press Up to Begin the Timer");
 	displayString (7,"Press Up Again to Stop the Timer");
-	while(!getButtonPress(UP_BUTTON)) 
+	while(!getButtonPress(UP_BUTTON))
 	{
 	}
-    while(getButtonPress(UP_BUTTON)) 
+    while(getButtonPress(UP_BUTTON))
 	{
 	}
 	eraseDisplay();
 	time1[T1]=0;
-	while(!getButtonPress(UP_BUTTON)) 
+	while(!getButtonPress(UP_BUTTON))
 	{
 	}
-    while(getButtonPress(UP_BUTTON)) 
+    while(getButtonPress(UP_BUTTON))
 	{
 	}
 	displayString (5, "Your time is %f", time1[T1]/1000.0);
 }
 
 // test mode function
-void testMode() 
-{ 
+void testMode()
+{
 // timer
 timer();
 
 // file for answers
 	TFileHandle fileIn;
-	bool fileOkay = openReadPC(fileIn, “answers.txt”);
-	if (!fileOkay) 
+	bool fileOkay = openReadPC(fileIn, "answers.txt");
+	if (!fileOkay)
 	{
-		displayString(5,”Error!”);
+		displayString(5,"Error!");
 		wait1Msec(5000);
 	}
-	else 
+	else
 	{
 		int questionNum=0;
 		int score = 0;
@@ -176,9 +181,9 @@ timer();
 		//drive and scan
 		driveStop();
 		float grade=float(score)/questionNum;
-		displayString (5, “You scored %d/%d, score, questionNum);
-			displayString( (6, “%f”, grade)
-			if (grade<60) 
+		displayString (5, �??You scored %d/%d, score, questionNum);
+			displayString( (6, �??%f�?�, grade)
+			if (grade<60)
 			{
 				fireBullet(); // need to create this function
 			}
@@ -188,7 +193,8 @@ return;
 
 
 // main function .....
-#include “PC_FileIO.c”
+#include "PC_FileIO.c"
+*/
 
 task main ()
 {
@@ -196,38 +202,40 @@ task main ()
 
 	int done=1;
 
-	while (done!=0) 
+	int mode = -1;
+	int lapCount= 0;
+	while (done!=0)
 	{
 		bool modeInterface=true;
-		while (modeInterface==true) 
+		while (modeInterface==true)
 		{
-			int mode = modeChoose();
+			mode = modeChoose();
 			modeInterface=false;
 		}
 
-		while (mode==1) {
-			// something is defo wrong its not making sense in my head
-			int lapCount=0;
-			studyMode(int(colorRed), lapCount);
-			if (lapCount>2) {
+		while (mode == 1)
+			{
+
+			studyMode((int)colorGreen, (int)colorBlack, lapCount);
+			if (lapCount == 2) {
 				done=0;
 			}
 		}
 
-		while (mode==2) 
+		while (mode==2)
 		{
 			testMode();
 			time1[T2]=0;
-			if ((time1[T2]/1000.)>120) 
+			if ((time1[T2]/1000)>120)
 			{
 				done=0;
 			}
-			
 		}
 
-		// press enter button to go back to the menu this defo doesnt work like i want it so that at any point if u ress it it goes back to the 
+
+		// press enter button to go back to the menu this defo doesnt work like i want it so that at any point if u ress it it goes back to the
 		// main menu
-		if (getButtonPress==ENTER_BUTTON) 
+		if(getButtonPress(ENTER_BUTTON) == true)
 		{
 			modeInterface=true;
 		}
